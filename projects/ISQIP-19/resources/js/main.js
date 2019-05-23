d9a4a72bf3c124fb4897bd296dc352bc985e9470
jack <imakhilravindran@gmail.com>
2019-05-23 19:33:42 +0530

    Changes made to project (testing)

--- projects/ISQIP-19/resources/js/main.js
+++ projects/ISQIP-19/resources/js/main.js
@@ -26,6 +26,22 @@ $(document).ready(()=> {
             $(".sidebar").fadeOut(300);
         }
     });
+
+    // side bar active change
+    $(window).on('hashchange load', ()=>{
+        let currentPage = window.location.hash.substring(1).slice(1);
+        if(currentPage == ''){
+            $('.menu li').removeClass('active');
+            $('.menu li div.selected').remove();
+            $('.home-btn').append('<div class="selected"></div>');
+        }else{
+            $('.menu li').removeClass('active');
+            $('.menu li div.selected').remove();
+            currentPage ='.' + currentPage + '-btn';
+            $(currentPage).append('<div class="selected"></div>');
+        }
+    });
+
 });
 $(document).mouseup(function (e) {
         if($(window).width() < 575.98){
@@ -34,5 +50,8 @@ $(document).mouseup(function (e) {
                 $(".mob-topbar, .content-side").css({opacity:1});
         }
     }
-  });
+});
+
+
+
 /////////////
