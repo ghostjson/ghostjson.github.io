28d6e5edd70b8fc8e3629cf382230824aaa00edc
jack <imakhilravindran@gmail.com>
2019-06-07 16:23:06 +0530

    Changes made to a project (test 3)

--- projects/fabplus/scripts/chat.js
+++ projects/fabplus/scripts/chat.js
@@ -1,7 +1,42 @@
-$(function () {
-    $(".chats, .bottom").hide();
-    $(".chatbox header").click(function (e) { 
-        e.preventDefault();
-        $(".chats, .bottom").toggle();
+// $(function () {
+    
+//     $(".chats, .bottom").hide();
+//     $(".chatbox header").click(function (e) { 
+//         e.preventDefault();
+//         if ($('body').width() > 650){
+//             $(".chats, .bottom").toggle();
+//         }else{
+//             $(".chats, .bottom,.chatbox header").toggle();
+//         }
+//     });
+//     // $(window).resize(function(){     
+// 	// if ($('body').width() <= 650){
+// 	// 	$(".chatbox header").click(function (e) { 
+// 	// 		e.preventDefault();
+// 	// 		$(".chats, .bottom,.chatbox header").fadeOut();
+// 	// 	});		
+
+// 	// }
+// });
+// });
+
+$(document).ready(function () {
+    $(window).resize(function () { 
+        if ($('body').width() > 650){
+            $(".chats, .bottom").hide();
+            $(".chatbox header").click(function (e) { 
+                e.preventDefault();
+                $(".chats, .bottom").toggle();
+            });
+        }
+        else{
+            console.log('hit');
+            $(".chats, .bottom, .chatbox header").hide();
+            $(".chatbox header").click(function (e) { 
+                e.preventDefault();
+                $(".chats, .bottom,.chatbox header").fadeOut();
+                $('.mobile-chat').fadeIn();
+            }); 
+        }
     });
 });
--- projects/fabplus/scripts/script.js
+++ projects/fabplus/scripts/script.js
@@ -7,3 +7,19 @@ $("a[href^='#']").click(function(e) {
 		scrollTop: position - 20
 	},600 );
 });
+
+$(document).ready(function () {
+	// mobile menu drop down
+	$(".menu-menu-btn").click(function (e) { 
+		e.preventDefault();
+		$('.dropdown-menu').fadeToggle();
+	});
+
+	//mobile chat button
+	$('.mobile-chat').click(function (e) { 
+		e.preventDefault();
+		$(".chats, .bottom,.chatbox header").fadeIn();
+		$(this).fadeOut();
+	});
+});
+
